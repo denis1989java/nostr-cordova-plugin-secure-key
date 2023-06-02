453f449f7701f54d3d0fb37035b7c91144867435
denis1989 <denis1989@bk.ru>
2023-06-02 09:59:19 +0300

    code review

---
+++ src/android/ModalActivity.java
@@ -0,0 +1,24 @@
+package com.nostr.plugin;
+
+import org.apache.cordova.CordovaActivity;
+import android.os.Bundle;
+
+public class ModalActivity extends CordovaActivity
+{
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    this.overridePendingTransition(R.anim.bottom_in, R.anim.hold);
+    super.init();
+
+    String url = getIntent().getStringExtra(Modal.PARAM_LOAD_URL);
+    super.loadUrl(url);
+  }
+
+  @Override
+  public void finish() {
+    super.finish();
+    this.overridePendingTransition(R.anim.diagonaltranslate,R.alpha);
+  }
+}
