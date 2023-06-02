88f1ab91bdb0dce47959310af21c4111279b29c5
denis1989 <denis1989@bk.ru>
2023-06-02 10:03:56 +0300

    code review
    (cherry picked from commit 1ba4167bd9e2cabae97e57197372fddec10dc1b7)

--- src/android/nostr.java
+++ src/android/nostr.java
@@ -14,7 +14,7 @@ import android.util.Log;
 import android.util.Base64;
 import android.security.KeyPairGeneratorSpec;
 import android.os.Build;
-com.nostr.plugin.ModalActivity;
+import com.nostr.plugin.ModalActivity;
 
 
 import java.security.*;
