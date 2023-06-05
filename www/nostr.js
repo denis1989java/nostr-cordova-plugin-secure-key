4e754401153330c8786045351050a018cce192c9
denis1989 <denis1989@bk.ru>
2023-06-05 15:55:44 +0300

    code review

--- www/nostr.js
+++ www/nostr.js
@@ -3,21 +3,21 @@ var exec = require('cordova/exec');
 var nostr = {
     serviceName: "nostr",
 
-    signEvent: function(success, error, key, value) {
+    signEvent: function (success, error, key, value) {
         exec(success, error, this.serviceName, "signEvent", [key, value]);
     },
 
-    getPublicKey: function(success, error, key) {
-        exec(success, error, this.serviceName, "getPublicKey", [key]);
+    getPublicKey: async function (success, error, key) {
+        await exec(success, error, this.serviceName, "getPublicKey", [key]);
     },
-    getPublicKey1: function(key) {
+    getPublicKey1: function (key) {
         exec(null, null, this.serviceName, "getPublicKey1", [key]);
     },
-    close: function(data) {
-        exec(null, null, this.serviceName, 'close', [ data ]);
+    close: function (data) {
+        exec(null, null, this.serviceName, 'close', [data]);
     },
-    successCallback: function(event) {
-        exec(null, null, this.serviceName, 'successCallback', [ event ]);
+    successCallback: function (event) {
+        exec(null, null, this.serviceName, 'successCallback', [event]);
     }
 
 };
