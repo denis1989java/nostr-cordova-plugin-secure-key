ebf5b022c8c7af66735e9776fe3c4d68f5281a06
denis1989 <denis1989@bk.ru>
2023-06-12 14:48:29 +0300

    added new libs

--- src/android/Utils.java
+++ src/android/Utils.java
@@ -1,19 +1,17 @@
 package android;
 
-import java.security.MessageDigest;
-
 public class Utils {
 
-  private final static MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+  //private final static MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
 
 
-  public ByteArray privkeyCreate (){
+/*  public ByteArray privkeyCreate (){
     ByteArray bytes = new ByteArray(32);
     random.nextBytes(bytes);
     return bytes
   }
 
   public pubkeyCreate(privKey: ByteArray) =
-          secp256k1.pubKeyCompress(secp256k1.pubkeyCreate(privKey)).copyOfRange(1, 33)
+          secp256k1.pubKeyCompress(secp256k1.pubkeyCreate(privKey)).copyOfRange(1, 33)*/
 
 }
