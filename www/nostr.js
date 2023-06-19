c166a33792b12c728f8571f7655f7e6a6b81af0d
denis1989 <denis1989@bk.ru>
2023-06-19 08:37:31 +0300

    changed files and package structure

--- www/nostr.js
+++ www/nostr.js
@@ -24,9 +24,11 @@ function onDeviceReady() {
             return new Promise((resolve, reject) => {
                 cordova.plugins.nostr.getPublicKey(
                     function (res) {
+                        console.log(JSON.stringify(res))
                         resolve(res.pubKey.replaceAll("\"", ""))
                     },
                     function (error) {
+                        console.log(JSON.stringify(error))
                         reject(error)
                     }
                 )
@@ -36,9 +38,11 @@ function onDeviceReady() {
             return new Promise((resolve, reject) => {
                 cordova.plugins.nostr.signEvent(
                     function (res) {
+                        console.log(JSON.stringify(res))
                         resolve(res)
                     },
                     function (error) {
+                        console.log(JSON.stringify(error))
                         reject(error)
                     },
                     msg
